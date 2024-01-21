6901ac25c9dd3f8c2a7bf2fa9ef366424a5b281b
sojinkim <sophie2dev@gmail.com>
2024-01-21 17:19:41 +0900

    chore: initial setup

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <q-layout>
+    <the-header />
+    <q-page-container>
+      <slot />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style scoped></style>
