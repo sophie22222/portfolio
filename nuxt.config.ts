5edc89ca7bee3e5338ef375f6edc02036b71e809
sojinkim <sophie2dev@gmail.com>
2024-02-05 16:48:10 +0900

    chore: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Sojin's Portfolio"
+    }
+  },
   modules: ['nuxt-quasar-ui']
 });
